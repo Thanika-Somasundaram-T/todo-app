5302b5d7a3c08831f9aec1adb404cd1ababca069
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-05-28 19:36:43 +0530

    Disabled add button when input is null.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<div>{ ToggleAllButton() }{ InputBox() }{ AddButton() }</div>
+			<div>{ ToggleAllButton() } { InputBox() } { AddButton() }</div>
 			<div>{ TodoList() }</div>
 			<div>{ ClearCompletedButton() }</div>
 			<div>{ FilterBar() }</div>
--- src/components/addButton.js
+++ src/components/addButton.js
@@ -2,6 +2,11 @@ import { React } from 'react';
 import context from '../core/context';
 
 const AddButton = () =>
-	<button onClick={ context.actions.addTodo }>Add</button>;
+	<button
+		disabled={ context.state.input === '' }
+		onClick={ context.actions.addTodo }
+	>
+		+
+	</button>;
 
 export default AddButton;
