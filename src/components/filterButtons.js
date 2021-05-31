2b8c5819eedb45e90d0054045faa34b1a398f237
rahulragava <rahulragava3@gmail.com>
2021-05-31 20:05:51 +0530

    Added styles to filter bar.

--- src/components/filterButtons.js
+++ src/components/filterButtons.js
@@ -2,15 +2,23 @@
 import { React } from 'react';
 import context from '../core/context';
 
+const sharedStyle = {
+	borderRadius: '8px',
+	margin: '5px',
+	width: '100px',
+};
+
 const trueStyle = {
+	...sharedStyle,
 	background: 'grey',
 	color: 'white',
 };
 
 const defaultStyle = {
+	...sharedStyle,
 	background: '',
-
 };
+
 const FilterButtons = (filterName) => {
 	const checkFilter
 	= context.state.filter === filterName ? trueStyle : defaultStyle;
